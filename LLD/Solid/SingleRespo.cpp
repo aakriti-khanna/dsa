a86a0329f56e6cc71011d0a66cd2ba715ec0a2ad
“aakriti <aakritikhanna206@gmail.com>
2024-07-25 23:39:10 +0530

    second commit

--- LLD/Solid/SingleRespo.cpp
+++ LLD/Solid/SingleRespo.cpp
@@ -30,7 +30,7 @@ class Sparrow : public Bird ,public flyable{
 class Ostrich :public Bird{
       public:
     void display() const override{
-        std::cout<<"I am Ostrich"<<std:: endl;
+        // std::cout<<"I am Ostrich"<<std:: endl;
     }
 
 };
